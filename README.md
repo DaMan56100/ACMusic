7f1291fda9aec6dee28b5971bda3d382d8354c8a
Daman56100 <samcraft792@gmail.com>
2020-05-01 16:49:01 +0100

    Switching to JavaFX

--- README.md
+++
@@ -1,3 +0,0 @@
-# ACMusic
- 
-But like, as a java SWING program
--- src/Main.java
+++
@@ -1,8 +0,0 @@
-import main.java.forms.Player;
-
-public class Main {
-    public static void main(String[] args) {
-        Player player = new Player();
-        player.show();
-    }
-}
