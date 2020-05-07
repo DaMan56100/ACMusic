d3bfc9196d62b90371deec0c7980b4ab9e4c9416
Daman56100 <samcraft792@gmail.com>
2020-05-07 18:26:54 +0100

    still unworked

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,21 @@
 package sample;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.Label;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+
+import java.net.URL;
+
 public class Controller {
+    public Label lblHello;
+
+    public void sayHelloWorld(ActionEvent actionEvent) {
+        lblHello.setText("Hello World!");
+
+        URL mediaURL = getClass().getResource("/chime.wav");
+        Media testMp3 = new Media(mediaURL.toExternalForm());
+        MediaPlayer mediaPlayer = new MediaPlayer(testMp3);
+        mediaPlayer.setAutoPlay(true);
+    }
 }
