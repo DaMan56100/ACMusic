736bedfa29a729a9bf3a0a11f851062a6deda6ff
Daman56100 <samcraft792@gmail.com>
2020-05-08 12:07:04 +0100

    now includes the workspace.xml

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -5,16 +5,18 @@ import javafx.scene.control.Label;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
 
+import java.io.File;
 import java.net.URL;
 
 public class Controller {
     public Label lblHello;
 
     public void sayHelloWorld(ActionEvent actionEvent) {
-        lblHello.setText("Hello World!");
+        //lblHello.setText("Hello World!");
 
-        URL mediaURL = getClass().getResource("/chime.wav");
-        Media testMp3 = new Media(mediaURL.toExternalForm());
+        URL mediaURL = getClass().getResource("/chime2.wav");
+        //Media testMp3 = new Media(mediaURL.toExternalForm());
+        Media testMp3 = new Media(new File("/home/sam/Documents/ACMusic/chime2.wav").toURI().toString());
         MediaPlayer mediaPlayer = new MediaPlayer(testMp3);
         mediaPlayer.setAutoPlay(true);
     }
