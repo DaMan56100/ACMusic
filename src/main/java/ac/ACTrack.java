39f882ade9870c3c9a612194c0e7595ea0438d13
DaMan56100 <43971727+DaMan56100@users.noreply.github.com>
2020-05-16 11:35:52 +0100

    An ACTrack is now no longer in charge of playing itself, that job was given to the Sound Player. It now just contains information about... the track

--- src/main/java/ac/ACTrack.java
+++ src/main/java/ac/ACTrack.java
@@ -3,11 +3,10 @@ package main.java.ac;
 import javax.sound.sampled.Clip;
 
 public interface ACTrack {
-    final String BELL_PATH = "/tracks/chime.wav";
-    final String TRACKS_CLEAR_DIRECTORY = "/tracks/clear/";
+    String BELL_PATH = "/tracks/chime.wav";
+    String TRACKS_CLEAR_DIRECTORY = "/tracks/clear/";
 
-    public int volume = 1;
-
-    void start();
-    void stop();
+    ACClipWrapper getIntroClip();
+    ACClipWrapper getMainClip();
+    boolean doesLoopMain();
 }
