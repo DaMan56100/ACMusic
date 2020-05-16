0d25f16665e0eeb2cf8b1135aeeaecdd0806739d
DaMan56100 <43971727+DaMan56100@users.noreply.github.com>
2020-05-16 11:34:07 +0100

    Created Sound Player, which is an interface between my needs and the raw java sound playing libraries

---
+++ src/main/java/ac/ACSoundPlayer.java
@@ -0,0 +1,64 @@
+package main.java.ac;
+
+import main.java.ClipUtils;
+
+import javax.sound.sampled.Clip;
+import javax.sound.sampled.LineEvent;
+import java.util.*;
+
+public class ACSoundPlayer {
+    ACClipWrapper currentClip;
+    float volume;
+
+    public float getVolume() {
+        return volume;
+    }
+
+    public void setVolume(float volume) {
+        this.volume = volume;
+        if (Objects.nonNull(currentClip)) ClipUtils.setVolumeOfClip(currentClip.clip,volume);
+    }
+
+    public ACSoundPlayer() {
+        volume = ClipUtils.DEFAULT_VOLUME;
+    }
+
+    public void playTrack(ACTrack track) {
+        System.out.println(String.format("intro: %s, main: %s",track.getIntroClip(),track.getMainClip()));
+        playClipSequence(
+                track.getIntroClip(),
+                track.getMainClip()
+        );
+    }
+
+    private void playClipSequence(ACClipWrapper... clips) {
+        Queue<ACClipWrapper> clipQueue = new LinkedList<>(Arrays.asList(clips));
+        playClipSequence(clipQueue);
+    }
+
+    private void playClipSequence(Queue<ACClipWrapper> clipQueue) {
+        ACClipWrapper firstClip = clipQueue.remove();
+        if (Objects.nonNull(firstClip)) {
+            firstClip.clip.addLineListener(event -> {
+                // at stop, play the next clip (if there is one);
+                if (event.getType().equals(LineEvent.Type.STOP) && !clipQueue.isEmpty()) {
+                    playClipSequence(clipQueue);
+                }
+            });
+            playClip(firstClip);
+        } else { // if clip is null, skip to next one
+            playClipSequence(clipQueue);
+        }
+    }
+
+    private void playClip(ACClipWrapper clip) {
+        currentClip = clip;
+        ClipUtils.setVolumeOfClip(clip.clip,volume);
+        if (clip.loop) {
+            clip.clip.loop(Clip.LOOP_CONTINUOUSLY);
+        } else {
+            clip.clip.start();
+        }
+        System.out.println("Playing: " + clip.clip.toString());
+    }
+}
