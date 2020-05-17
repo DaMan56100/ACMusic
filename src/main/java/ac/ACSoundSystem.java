c146bbd35cf45288c005daec038b1d90130390e2
DaMan56100 <43971727+DaMan56100@users.noreply.github.com>
2020-05-17 11:31:30 +0100

    Created fadeOutAtNextHour() This function creates a thread which, at the last minute of the hour, fades out, plays the bell chime and then call play() again to start the next hour.

--- src/main/java/ac/ACSoundSystem.java
+++ src/main/java/ac/ACSoundSystem.java
@@ -14,6 +14,7 @@ public class ACSoundSystem {
             int hour = LocalDateTime.now().getHour();
             ACHourTrack hourTrack = new ACHourTrack(hour);
             soundPlayer.playTrack(hourTrack);
+            fadeOutAtNextHour();
         } catch (ACTrackGenerationException ex) {
             ex.printStackTrace();
         }
@@ -26,5 +27,44 @@ public class ACSoundSystem {
         } catch (ACTrackGenerationException ex) {
             ex.printStackTrace();
         }
+    }
+
+    public void fadeOutAtNextHour() {
+        new Thread(() -> {
+            boolean fadingOut = false;
+            while (!fadingOut) {
+                LocalDateTime currentTime = LocalDateTime.now();
+                if (
+                     currentTime.getMinute() == 59 &&
+                     currentTime.getSecond() >= 52
+                ) fadingOut = true;
+                else {
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+
+            float currentVolume = soundPlayer.getVolume();
+            for (float i = 1; i >= 0.2; i-=0.001f) {
+                soundPlayer.setVolume(currentVolume * i);
+                try {
+                    Thread.sleep(10);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            soundPlayer.setVolume(currentVolume);
+            try {
+                soundPlayer.playTrack(new ACBellTrack());
+                Thread.sleep(15000);
+            } catch (ACTrackGenerationException | InterruptedException e) {
+                e.printStackTrace();
+            }
+            play();
+        }).start();
     }
 }
