d61b2f472978479f0d811e74b3804ca8f1708ce7
DaMan56 <43971727+DaMan56100@users.noreply.github.com>
2020-05-18 08:06:56 +0100

    Reverted the bad commits
    Let's ignore what happened last night... Note to self, never just press the 'merge' button and assume it'll all be ok.

---
+++ src/main/java/ac/IACTrackInfoDisplayer.java
@@ -0,0 +1,5 @@
+package main.java.ac;
+
+public interface IACTrackInfoDisplayer {
+    void setTrackDetails(String name);
+}
--- src/main/java/forms/Player.java
+++ src/main/java/forms/Player.java
@@ -1,52 +1,30 @@
 package main.java.forms;
 
-import javax.sound.sampled.*;
-import javax.swing.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.io.File;
-import java.io.IOException;
+import main.java.ClipUtils;
+import main.java.ac.*;
 
-public class Player {
+import javax.swing.*;
+import java.time.LocalDateTime;
+
+public class Player implements IACTrackInfoDisplayer {
     private JFrame displayFrame;
+    ACSoundSystem soundSystem = new ACSoundSystem();
 
     public Player() {
         displayFrame = new JFrame("AC Music Player");
         displayFrame.setContentPane(panMain);
         displayFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         displayFrame.setSize(400,200);
+        numVolume.setValue((int)(ClipUtils.DEFAULT_VOLUME * 100));
 
-        btnPlay.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                try {
-                    System.out.println("All mixers:");
-                    Mixer.Info foundInfo = null;
-                    for (Mixer.Info m : AudioSystem.getMixerInfo()) {
-                        System.out.println(m.getName());
-                        if (m.getName().equals("HDMI [plughw:0,8]")) {
-                            foundInfo = m;
-                        }
-                    }
+        btnPlay.addActionListener(e -> soundSystem.play());
+        btnStop.addActionListener(e -> soundSystem.pause());
+        numVolume.addChangeListener(e -> soundSystem.setVolume(numVolume.getValue() / 100f));
+        soundSystem.setTrackInfoDisplayer(this);
+    }
 
-                    System.out.println("Default mixer: " + AudioSystem.getMixer(null).getMixerInfo());
-                    //System.out.println("  Using mixer: " + foundInfo.toString());
-
-                    AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(new File("bsc.wav"));
-                    Clip clip = AudioSystem.getClip();
-                    clip.open(audioInputStream);
-                    clip.start();
-
-                    System.out.println((new File("").getAbsolutePath()));
-                } catch (UnsupportedAudioFileException ex) {
-                    ex.printStackTrace();
-                } catch (IOException ex) {
-                    ex.printStackTrace();
-                } catch (LineUnavailableException ex) {
-                    ex.printStackTrace();
-                }
-            }
-        });
+    public void setTrackDetails(String name) {
+        lblTrackDetails.setText("Now Playing: " + name);
     }
 
     public void show() {
@@ -60,4 +38,5 @@ public class Player {
     private JSlider numVolume;
     private JPanel panInterface;
     private JPanel panPlayerControls;
+    private JLabel lblTrackDetails;
 }
